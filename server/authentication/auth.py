e1c8e23e3ff80fa6963e1f5a0fbe1b4c0792b6d3
archie955 <archiesmedley@gmail.com>
2026-02-16 18:51:08 +0000

    add auth get current user dependency injection

--- server/authentication/auth.py
+++ server/authentication/auth.py
@@ -3,6 +3,10 @@ from fastapi.security import OAuth2PasswordBearer
 from server.utils.config import settings
 from datetime import datetime, timezone, timedelta
 from server.models.schemas import Token, TokenData
+from fastapi import Depends, HTTPException, status
+from server.database.database import get_db
+from sqlalchemy.orm import Session
+import server.models.models as models
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
 
@@ -16,14 +20,22 @@ def create_access_token(data: dict):
     expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
 
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode,
+                             SECRET_KEY,
+                             algorithm=ALGORITHM
+                             )
 
     return encoded_jwt
 
-def verify_access_token(token: str, credentials_exception):
+def verify_access_token(token: str,
+                        credentials_exception
+                        ):
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        id = str(payload.get("user_id"))
+        payload = jwt.decode(token,
+                             SECRET_KEY, 
+                             algorithms=[ALGORITHM]
+                             )
+        id: str = payload.get("sub")
 
         if id is None:
             raise credentials_exception
@@ -34,3 +46,22 @@ def verify_access_token(token: str, credentials_exception):
     except jwt.exceptions.InvalidTokenError:
         raise credentials_exception
     
+async def get_current_user(token: str = Depends(oauth2_scheme),
+                           db: Session = Depends(get_db)
+                           ):
+    credentials_exceptions = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                           detail=f"Could not validate credentials",
+                                           headers={"WWW-Authenticate": "Bearer"}
+                                           )
+    token = verify_access_token(token,
+                                credentials_exception=credentials_exceptions
+                                )
+
+    user = db.query(models.User).filter(models.User.id == token.id).first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail=f"User does not exist"
+                            )
+    
+    return user
