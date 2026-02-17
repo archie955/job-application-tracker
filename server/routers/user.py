de7dc7421dcb064a1799fc30fe867c3fcc483cc7
archie955 <archiesmedley@gmail.com>
2026-02-17 18:45:24 +0000

    ensure consistent urls for register

--- server/routers/user.py
+++ server/routers/user.py
@@ -9,7 +9,7 @@ from fastapi.responses import JSONResponse
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
-@router.post("/create", status_code=status.HTTP_201_CREATED, response_model=JSONResponse)
+@router.post("/register", status_code=status.HTTP_201_CREATED, response_model=JSONResponse)
 def create_user(user: schemas.UserCreate,
                 db: Session = Depends(get_db)
                 ):
---
+++ web/pages/Register.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { LoginForm } from "../components/LoginForm"
+
+const Register = () => {
+    const { register } = useAuth()
+    const navigate = useNavigate()
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [showPassword, setShowPassword] = useState(false)
+    const [error, setError] = useState("")
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            await register(email, password)
+            navigate("/home")
+        } catch (err) {
+            setError("Invalid credentials")
+        }
+    }
+
+    return (
+        <div>
+        <h2>{error}</h2>
+        <LoginForm
+            loginFunction={handleSubmit}
+            email={email}
+            setEmail={setEmail}
+            password={password}
+            setPassword={setPassword}
+            showPassword={showPassword}
+            setShowPassword={setShowPassword}
+            />
+        </div>
+    )
+}
+
+export default Register
