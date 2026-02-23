62740262afeafa7b6ef0be78ef0662ed19cebde6
archie955 <archiesmedley@gmail.com>
2026-02-23 19:08:26 +0000

    fix origins to use .env

--- server/main.py
+++ server/main.py
@@ -5,10 +5,7 @@ from server.utils.config import settings
 
 app = FastAPI()
 origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:5173"
+    settings.backend_url
 ]
 
 app.add_middleware(
