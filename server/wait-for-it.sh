47e61ea5f3e02623216b1682bd5cce49eef0d7f2
archie955 <archiesmedley@gmail.com>
2026-02-26 22:34:28 +0000

    finally working, successful dockerisation

---
+++ server/wait-for-it.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# wait-for-it.sh
+
+# This script waits until a TCP host and port are available.
+
+TIMEOUT=15
+HOST="$1"
+PORT="$2"
+shift 2
+
+
+echo "Waiting for $HOST:$PORT to be available..."
+
+for i in `seq $TIMEOUT`; do
+    nc -z "$HOST" "$PORT" && echo "$HOST:$PORT is available" && exec "$@"
+    echo "$HOST:$PORT is not yet available, waiting..."
+    sleep 1
+done
+
+echo "Timeout reached. $HOST:$PORT is not available."
+exit 1
