149c4d246fb970a435ff4ff1a53d7db471d2345f
archie955 <archiesmedley@gmail.com>
2026-02-27 20:08:41 +0000

    altered logging_config to streamhandler for docker

--- server/utils/logging_config.py
+++ server/utils/logging_config.py
@@ -1,11 +1,8 @@
 import logging
-import os
 
 def setup_logging():
-    log_level = os.getenv("LOG_LEVEL", "INFO").upper()
-
     logging.basicConfig(
-        level=log_level,
+        level=logging.INFO,
         format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
         handlers=[logging.StreamHandler()]
     )
