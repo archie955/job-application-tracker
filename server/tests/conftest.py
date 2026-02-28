440f22993b0161d968d2ec7a46a2ab2c55792585
archie955 <archiesmedley@gmail.com>
2026-02-28 17:24:14 +0000

    create testing config create health check test create testing database yml

---
+++ server/tests/conftest.py
@@ -0,0 +1,40 @@
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from fastapi.testclient import TestClient
+
+from main import app
+from database.database import get_db
+from models.models import Base
+
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost:5433/test_db" # just a test database that gets made for tests then dropped immediately
+
+
+@pytest.fixture(scope="session")
+def engine():
+    engine = create_engine(url=SQLALCHEMY_DATABASE_URL)
+    Base.metadata.create_all(bind=engine)
+    yield engine
+    Base.metadata.drop_all(bind=engine)
+
+@pytest.fixture(scope="function")
+def db(engine):
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    db = SessionLocal()
+    yield db
+    db.close()
+
+@pytest.fixture(scope="function")
+def client(db):
+    def override_get_db():
+        try:
+            yield db
+        finally:
+            pass
+    
+    app.dependency_overrides[get_db] = override_get_db
+
+    with TestClient(app) as c:
+        yield c
+
+    app.dependency_overrides.clear()
