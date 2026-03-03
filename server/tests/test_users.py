03c1bd83c9109fe5ab57d583802051cade964651
archie955 <archiesmedley@gmail.com>
2026-03-03 00:15:02 +0000

    refactored code to use auth headers function

--- server/tests/test_users.py
+++ server/tests/test_users.py
@@ -1,3 +1,10 @@
+import pytest
+
+def auth_headers(authenticated_user):
+    return {
+        "Authorization": f"Bearer {authenticated_user['access_token']}"
+    }
+
 def test_registration(client):
     response = client.post(
         "/users/register",
@@ -66,9 +73,7 @@ def test_incorrect_login(client):
 
 def test_logout(client, authenticated_user):
     response = client.get("/users/logout",
-                          headers={
-                          "Authorization": f"Bearer {authenticated_user['access_token']}"
-                          }
+                          headers=auth_headers(authenticated_user)
                           )
     assert response.status_code == 200
     assert response.json()["message"] == "successfully logged out"
@@ -92,18 +97,14 @@ def test_missing_refresh(client):
 
 def test_delete(client, authenticated_user):
     response = client.delete("/users/me",
-                             headers={
-                                 "Authorization": f"Bearer {authenticated_user['access_token']}"
-                             }
+                             headers=auth_headers(authenticated_user)
                              )
     assert response.status_code == 204
     
 def test_update_email(client, authenticated_user):
     response = client.put("/users/me/email",
         json={"email": "newemail@example.com"},
-        headers={
-            "Authorization": f"Bearer {authenticated_user['access_token']}"
-        }
+        headers=auth_headers(authenticated_user)
         )
 
     assert response.status_code == 200
@@ -112,9 +113,7 @@ def test_update_email(client, authenticated_user):
 def test_update_same_email(client, authenticated_user):
     response = client.put("/users/me/email",
         json={"email": authenticated_user["email"]},
-        headers={
-            "Authorization": f"Bearer {authenticated_user['access_token']}"
-        }
+        headers=auth_headers(authenticated_user)
         )
 
     assert response.status_code == 400
@@ -122,9 +121,7 @@ def test_update_same_email(client, authenticated_user):
 def test_update_password(client, authenticated_user):
     response = client.put("/users/me/password",
         json={"password": "newsecurepassword"},
-        headers={
-            "Authorization": f"Bearer {authenticated_user['access_token']}"
-        }
+        headers=auth_headers(authenticated_user)
         )
 
     assert response.status_code == 200
@@ -132,9 +129,7 @@ def test_update_password(client, authenticated_user):
 def test_update_same_password(client, authenticated_user):
     response = client.put("/users/me/password",
         json={"password": authenticated_user["password"]},
-        headers={
-            "Authorization": f"Bearer {authenticated_user['access_token']}"
-        }
+        headers=auth_headers(authenticated_user)
         )
 
     assert response.status_code == 400
